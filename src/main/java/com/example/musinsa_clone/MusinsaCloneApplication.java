960b41659c2677ee2019c29fe2a65dba7c7bfdc8
전재연 <sunrox1107@gmail.com>
2024-10-08 20:36:48 +0900

    first commit

---
+++ src/main/java/com/example/musinsa_clone/MusinsaCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.musinsa_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/musinsa_clone/MusinsaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musinsa_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
