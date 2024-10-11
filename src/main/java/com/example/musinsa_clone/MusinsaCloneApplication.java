152139f6860397dbe835d96fdd65471249c51d58
전재연 <sunrox1107@gmail.com>
2024-10-11 09:23:21 +0900

    [feat] enableJpaAuditing 어노테이션 추가

--- src/main/java/com/example/musinsa_clone/MusinsaCloneApplication.java
+++ src/main/java/com/example/musinsa_clone/MusinsaCloneApplication.java
@@ -2,8 +2,10 @@ package com.example.musinsa_clone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MusinsaCloneApplication {
 
     public static void main(String[] args) {
