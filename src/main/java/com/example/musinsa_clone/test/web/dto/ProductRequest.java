f6c9075885969951a0e800c9fd0cd20e78fca615
전재연 <sunrox1107@gmail.com>
2024-10-13 13:17:05 +0900

    [feat] 테스트용 DTO 및 컨트롤러 개발

---
+++ src/main/java/com/example/musinsa_clone/test/web/dto/ProductRequest.java
@@ -0,0 +1,11 @@
+package com.example.musinsa_clone.test.web.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductRequest {
+    private String name;
+    private String description;
+    private Double price;
+    private Long categoryId; // 카테고리 ID
+}
