2f57300d4f05e27e4e7555e17902bf8efcd3092c
전재연 <sunrox1107@gmail.com>
2024-10-14 20:54:32 +0900

    [fix] 필요없는 클래스 삭제

--- src/main/java/com/example/musinsa_clone/test/testRepository/TestCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.musinsa_clone.test.testRepository;
-
-import com.example.musinsa_clone.test.testEntity.TestCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestCategoryRepository extends JpaRepository<TestCategory,Long> {
-}
