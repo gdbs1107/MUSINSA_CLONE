ac08be00947326aaec1bef4bb350c199152c3b46
전재연 <sunrox1107@gmail.com>
2024-10-19 23:50:12 +0900

    [feat] 이넘 클래스 주석 추가

--- src/main/java/com/example/musinsa_clone/domain/enumClass/Grade.java
+++ src/main/java/com/example/musinsa_clone/domain/enumClass/Grade.java
@@ -1,5 +1,10 @@
 package com.example.musinsa_clone.domain.enumClass;
 
 public enum Grade {
+
+    /* 개별 회원의 등급
+    * 포인트를 통해 등급이 상승합니다
+    * 등급별로 할인률이 상이합니다 */
+
     BRONZE,SILVER,GOLD,PLATINUM,DIAMOND
 }
--- src/main/java/com/example/musinsa_clone/domain/enumClass/SocialType.java
+++ src/main/java/com/example/musinsa_clone/domain/enumClass/SocialType.java
@@ -1,5 +1,8 @@
 package com.example.musinsa_clone.domain.enumClass;
 
 public enum SocialType {
+
+    /* 소셜로그인 구별 */
+
     GENERAL,KAKAO,NAVER
 }
