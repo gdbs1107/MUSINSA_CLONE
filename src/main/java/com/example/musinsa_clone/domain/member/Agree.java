75c570f86926cd9434645b0fa1f1086811e434a2
전재연 <sunrox1107@gmail.com>
2024-10-21 11:59:42 +0900

    [feat] 주석 추가

--- src/main/java/com/example/musinsa_clone/domain/member/Agree.java
+++ src/main/java/com/example/musinsa_clone/domain/member/Agree.java
@@ -14,15 +14,23 @@ public class Agree {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /* 14세 이상 동의
+    * 필수 값 */
     @Column(nullable = false)
     private Boolean ageAgree = Boolean.TRUE;
 
+    /* 무신사 이용약관 동의
+     * 필수 값 */
     @Column(nullable = false)
     private Boolean musinsaAgree = Boolean.TRUE;
 
+    /* 광고 동의
+     * 선택 값 */
     @Column(nullable = false)
     private Boolean adAgree;
 
+    /* 마케팅 광고 동의
+     * 선택 값 */
     @Column(nullable = false)
     private Boolean marketingAgree;
 
