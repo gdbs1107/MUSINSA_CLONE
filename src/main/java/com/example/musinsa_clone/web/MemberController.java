4fcaa04c0ec365fd154e293e7981455a9b7540ae
전재연 <sunrox1107@gmail.com>
2024-10-21 11:53:49 +0900

    [feat] dto 설정

--- src/main/java/com/example/musinsa_clone/web/MemberController.java
+++ src/main/java/com/example/musinsa_clone/web/MemberController.java
@@ -1,4 +1,26 @@
 package com.example.musinsa_clone.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
+
+    //회원가입
+    @PostMapping("/join")
+    public void join(@RequestBody Member member) {
+
+    }
+
+    //회원정보 수정
+
+    //회원탈퇴
+
+    //회원조회
+
+
 }
--- src/main/java/com/example/musinsa_clone/web/dto/MemberJoinDTO.java
+++ src/main/java/com/example/musinsa_clone/web/dto/MemberJoinDTO.java
@@ -1,4 +1,27 @@
 package com.example.musinsa_clone.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class MemberJoinDTO {
+
+    @Getter
+    public static class JoinRequestDTO{
+
+        //회원 약관동의
+
+
+
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class JoinResponseDTO{
+
+
+    }
 }
